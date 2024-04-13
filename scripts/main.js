b46957ba12da3e687b43b7215117913a9c308fe8
Jiuru Lyu <lvjiuru@hotmail.com>
2024-04-13 00:31:48 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -39,26 +39,26 @@ myImage.onclick = function () {
 }
 
 // Customized Welcoming Message for different Users
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    let myName = prompt("Please enter your name");
-    if (!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem("name", myName);
-        myHeading.textContent = "Welcome to my website, " + myName;
-    }
-}
-
-if (!localStorage.getItem("name")) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Welcome to my website, " + storedName;
-}
-
-myButton.onclick = function () {
-    setUserName();
-}
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+//
+// function setUserName() {
+//     let myName = prompt("Please enter your name");
+//     if (!myName) {
+//         myHeading.textContent = "Welcome to my website, Somebody";
+//     } else {
+//         localStorage.setItem("name", myName);
+//         myHeading.textContent = "Welcome to my website, " + myName;
+//     }
+// }
+//
+// if (!localStorage.getItem("name")) {
+//     setUserName();
+// } else {
+//     let storedName = localStorage.getItem("name");
+//     myHeading.textContent = "Welcome to my website, " + storedName;
+// }
+//
+// myButton.onclick = function () {
+//     setUserName();
+// }
